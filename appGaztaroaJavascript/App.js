0770a470e11db05836c3b476b4dba8819a64d4f8
Pablo Gonzalo <pablo.gonzalo556@gmail.com>
2025-03-26 16:22:24 +0100

    Final de la tercera práctica

--- appGaztaroaJavascript/App.js
+++ appGaztaroaJavascript/App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react'; 
 import { View } from 'react-native'; 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Campobase from './components/CampobaseComponent';
+import Campobase from './componentes/CampobaseComponent';
+
 
 export default function App() { 
   return ( 
